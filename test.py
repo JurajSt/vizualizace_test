ef99ac9fe244ba45530fe1713bafc660691cdbc5
JurajSt <struhar.juraj@gmail.com>
2018-09-30 20:52:30 +0200

    ...

--- test.py
+++ test.py
@@ -4,10 +4,11 @@ import pyexcel as pe
 import csv
 import re
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 import datetime
+import arcpy
 
-path = 'tvrdonice_124_2_21642_crop1_orez.csv'
+path = 'data/tvrdonice_124_2_21642_crop1_ps.csv'
 
 def csvReader(path):
     with open(path) as csvfile:
@@ -23,6 +24,9 @@ def csvReader(path):
             data_split.append(value_split)
     return data_split
 
+def runningMeanFast(x, N):
+    return np.convolve(x, np.ones((N,))/N)[(N-1):]
+
 read = csvReader(path)
 sheet = pe.Sheet(read)
 sheet.name_columns_by_row(0)
@@ -33,19 +37,23 @@ lon = map(float, sheet.column['LON'])
 #plt.plot(lon, lat, 'o')
 #plt.show()
 
+print header
 
 dataHeader = []
 for d in header[12:]:
-    date = datetime.datetime.strptime(d, '%d-%b-%Y')
+    date = d.split('-')
+
     dataHeader.append(date)
+
 
 for row in sheet.row:
 
     data = row[12:]
-    elements = numpy.array(map(float, data))
-    mean = numpy.mean(elements, axis=0)
-    sd = numpy.std(elements, axis=0)
+    elements = np.array(map(float, data))
 
+    """
+    mean = np.mean(elements, axis=0)
+    sd = np.std(elements, axis=0)
     final_list = [x for x in data if (x > mean - 2 * sd)]
     final_list = [x for x in final_list if (x < mean + 2 * sd)]
     #plt.plot(dataHeader, map(float, data), 'o')
@@ -53,7 +61,10 @@ for row in sheet.row:
     #plt.plot([dataHeader[0], dataHeader[-1]], [-2 * sd, -2 * sd], 'r')
     plt.boxplot(elements)
     plt.show()
-
     plt.hist(elements)
+    plt.show()"""
+    ma = runningMeanFast(elements, 10)
+    plt.plot(dataHeader, ma)
     plt.show()
+
 
